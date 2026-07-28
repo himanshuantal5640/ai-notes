065442dc25a4d7490473ae853494b504d9fc4f34
himanshu-antal <himanshuantal26@gmail.com>
2026-07-28 17:46:59 +0530

    ai notes project initialization

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/db');
+
+connectDB(); //connection to DB
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running on http://localhost:${PORT}`);
+})
