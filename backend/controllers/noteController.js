ea5a40e7d9f708785e2660f1556de51ebc9a5142
himanshu-antal <himanshuantal26@gmail.com>
2026-07-30 16:19:26 +0530

    CRUD Operation done

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -27,5 +27,101 @@ const createNotes = async(req,res)=>{
     }
 }
 
-module.exports = {createNotes};
+const getNotes = async (req,res) =>{
+    try{
+        const note = await Notes.find().sort({createdAt: -1});
+        if(!note){
+            return res.status(400).json({
+                success:false,
+                message:"Data not found"
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            message:"Data found",
+            data:note
+        });
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+
+const getSingleNotes = async(req,res) =>{
+    try{
+        const note = await Notes.findById(req.params.id);
+        if(!note){
+            return res.status(400).json({
+                success:false,
+                message:"Not found"
+            });
+        }
+        return res.status(200).json({
+            success:true,
+            message:"Data Found",
+            data:note
+        });
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+
+const updateNotes = async(req,res) =>{
+    try{
+        const {title,content} = req.body;
+        const note = await Notes.findById(req.params.id);
+        if(!note){
+            return res.status(400).json({
+                success:false,
+                message:"Data Not Found"
+            });
+        }
+        note.title = title || note.title;
+        note.content = content || note.content;
+        await note.save();
+        return res.status(200).json({
+            success:true,
+            message:"Data Updated",
+            data:note
+        });
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+const deleteNotes = async(req,res) =>{
+    try{
+        const note = await Notes.findById(req.params.id);
+        if(!note){
+            return res.status(404).json({
+                success:false,
+                message:"Data Not Found"
+            });
+        }
+        await note.deleteOne();
+        return res.status(200).json({
+            success:true,
+            message:"Data deleted successfully"
+        });
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+module.exports = {createNotes,getNotes,getSingleNotes,updateNotes,deleteNotes};
 
--- backend/routes/notesRoute.js
+++ backend/routes/notesRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { createNotes } = require('../controllers/noteController');
+const { createNotes, getNotes, getSingleNotes, updateNotes, deleteNotes } = require('../controllers/noteController');
 const router = express.Router();
 
 router.post('/',createNotes);
+router.get('/',getNotes);
+router.get('/:id',getSingleNotes);
+router.post('/:id',updateNotes);
+router.delete('/:id',deleteNotes);
 
 module.exports = router;
